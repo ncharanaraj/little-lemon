cc0713762fbc32590bf0df49b4188e2473dc5aed
Charanaraj N <goudacharanraj760@gmail.com>
2023-01-28 10:39:15 +0530

    structured footer with logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,29 @@
+import footerLogo from "../images/footer-logo.png"
+import Nav from "./Nav";
+
 function Footer(){
     return(
-        <></>
+        <>
+            <div>
+                <img src={footerLogo} alt="footer logo" />
+            </div>
+            <div>
+                <h4>Doormat Navigation</h4>
+                <Nav />
+            </div>
+            <div>
+                <h4>Contact</h4>
+                <li>Adress</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </div>
+            <div>
+                <h4>Social Media Links</h4>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Twitter</li>
+            </div>
+        </>
     );
 }
 
